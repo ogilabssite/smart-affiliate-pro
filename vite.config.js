7a9cce30e56070c9c0318850b7387ef98e54f42e
saji <saji06216@gmail.com>
2026-03-26 07:35:57 +0700

    Fix: Change base path to relative for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Konfigurasi standar untuk React di Vercel
 export default defineConfig({
   plugins: [react()],
-  base: '/', // TAMBAHKAN BARIS INI: Mengatasi error 404 pathing di Vercel
+  base: './', // <--- UBAH JADI INI (Titik Slash)
   server: {
     port: 3000,
   },
   build: {
     outDir: 'dist',
-    emptyOutDir: true, // Memastikan build selalu bersih
+    emptyOutDir: true, 
   }
 })
